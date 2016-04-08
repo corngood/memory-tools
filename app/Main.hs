faa53bd8899de228f8938fcc120177a4c4973287
David McFarland <davidm@hb-studios.com>
2016-04-08 16:45:46 -0300

    testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import System.Environment
+import Text.ParserCombinators.ReadP
+
+data Token = Token
+           deriving (Eq, Ord, Show, Read)
+
+validToken :: ReadP Token
+validToken = string "!$!" >> return Token
+
+token :: ReadP (Maybe Token)
+token = (Just <$> validToken) <++ (get >> return Nothing)
 
 main :: IO ()
-main = someFunc
+main = do
+  input <- (head <$> getArgs) >>= readFile
+  print $ (fst <$> (readP_to_S validToken input))
+  -- print $ take 5 $ fst $ head $ readP_to_S (many token) input
+  return ()
