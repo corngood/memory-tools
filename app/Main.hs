b1661599bd0e055ef8ead24b7962009b32a3c5b1
David McFarland <corngood@gmail.com>
2016-04-09 19:46:13 -0300

    Token parsing working

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,43 @@
 module Main where
 
+import Data.Word
 import System.Environment
 import Text.ParserCombinators.ReadP
+import Text.Read.Lex
 
-data Token = Token
+newtype Address = Address Word64
+                deriving (Eq, Ord, Show, Read)
+newtype Size = Size Word64
+             deriving (Eq, Ord, Show, Read)
+
+data Token = Allocate Address Size
+           | Deallocate Address
            deriving (Eq, Ord, Show, Read)
 
-validToken :: ReadP Token
-validToken = string "!$!" >> return Token
+address :: ReadP Address
+address = Address <$> readHexP
 
-token :: ReadP (Maybe Token)
-token = (Just <$> validToken) <++ (get >> return Nothing)
+size :: ReadP Size
+size = Size <$> readHexP
+
+validToken :: ReadP Token
+validToken = between (string "!$!") (char '\n') $
+  choice [ char '+' >> Allocate <$> address <*> (char ',' >> size)
+         , char '-' >> Deallocate <$> address
+         ]
+
+token :: ReadP Token
+token = validToken <++ (get >> token)
+
+tokenList :: String -> [Token]
+tokenList input = case readP_to_S token input of
+  [(x, rest)] -> x:tokenList rest
+  [] -> []
+  _ -> error "parse error"
 
 main :: IO ()
 main = do
   input <- (head <$> getArgs) >>= readFile
-  print $ (fst <$> (readP_to_S validToken input))
-  -- print $ take 5 $ fst $ head $ readP_to_S (many token) input
+  -- input <- getContents
+  print $ take 100 $ tokenList input
   return ()
